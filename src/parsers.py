993bf6e3e33eb36feb784c15897ba94fc85de9d4
bbuysschaert <bram.buysschaert@gmail.com>
2021-11-14 15:17:01 +0100

    Adding try-except statements in parser

--- src/parsers.py
+++ src/parsers.py
@@ -33,30 +33,36 @@ def parse_authorlist(_authorlist:str, **kwargs) -> list:
     Parse the string of authors to retrieve the list of individual authors
     """
     # Anticipated to result in length / 2 = number of authors
-    _temp = _authorlist.split(', ')
-    
-    # Some basic cleaning
-    _temp = [ii.replace('&', '').strip() for ii in _temp]
-    
-    # Differentiate to extract collaborations
-    if len(_temp) % 2 == 0:
-        _result = [', '.join([_temp[ii * 2], _temp[ii * 2 + 1]]) for ii in range(len(_temp) // 2)]
-    elif len(_temp) % 2 == 1:
-        _result = [', '.join([_temp[ii * 2], _temp[ii * 2 + 1]]) for ii in range(len(_temp) // 2 - 1)]
-        _result += [_temp[-1]]
-    
-    return _result
+    try:
+        _temp = _authorlist.split(', ')
+        
+        # Some basic cleaning
+        _temp = [ii.replace('&', '').strip() for ii in _temp]
+        
+        # Differentiate to extract collaborations
+        if len(_temp) % 2 == 0:
+            _result = [', '.join([_temp[ii * 2], _temp[ii * 2 + 1]]) for ii in range(len(_temp) // 2)]
+        elif len(_temp) % 2 == 1:
+            _result = [', '.join([_temp[ii * 2], _temp[ii * 2 + 1]]) for ii in range(len(_temp) // 2 - 1)]
+            _result += [_temp[-1]]
+        
+        return _result
+    except:
+        return _authorlist
 
 def parse_keywordlist(_keywords:str, **kwargs) -> list:
     """
     Parse the string of keywords to retrieve the list of keywords
     """
-    _result = _keywords.split(',')
-    
-    # Some basic cleaning
-    _result = [ii.strip() for ii in _result]
-    
-    return _result
+    try:
+        _result = _keywords.split(',')
+        
+        # Some basic cleaning
+        _result = [ii.strip() for ii in _result]
+        
+        return _result
+    except:
+        return _keywords
 
 def enrich_paperinfo_abstract(_paperinfos:list, _abstracts:dict) -> list:
     """
