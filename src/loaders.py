e58ab9aaf184610cee8928f8b384751993af6200
bbuysschaert <bram.buysschaert@gmail.com>
2022-01-04 15:12:31 +0100

    Separating writing the citations and paper info

--- src/loaders.py
+++ src/loaders.py
@@ -13,6 +13,33 @@ def write_data(_info:dict, _path='./', **kwargs):
                 )
     return
 
+def write_paperinfo(_info:dict, _path='./', **kwargs):
+    """
+    Write the info object of a paper as a json at _path.  The filename contains the bibcode
+    """
+    _filename = _info['bibcode'] + '_paperinfo.json'
+    with open(os.path.join(_path, _filename), 'w') as _out:
+        json.dump(_info,
+                _out,
+                indent=1
+                )
+    return
+
+def write_papercitations(_info:dict, _path='./', **kwargs):
+    """
+    Write the citations of a paper as a json at _path.  The filename contains the bibcode
+    """
+    _filename = _info['bibcode'] + '_citations.json'
+
+    # Subset the information
+    _info = {_key:_info[_key] for _key in ['bibcode', 'citations']}
+    with open(os.path.join(_path, _filename), 'w') as _out:
+        json.dump(_info,
+                _out,
+                indent=1
+                )
+    return
+
 def write_error(_bibcode:str, _path='./', **kwargs):
     """
     Write the bibcode to a separate file to trace which bibcodes could not be searched and parsed
